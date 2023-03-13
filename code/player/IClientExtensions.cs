93e5f688e5c930d17b330fdcd07d5c819687c35a
Ephememory <leetvipertm@gmail.com>
2023-03-13 02:31:12 -0500

    improve scoreboard

--- code/player/IClientExtensions.cs
+++ code/player/IClientExtensions.cs
@@ -17,4 +17,19 @@ public static class IClientExtensions
 	{
 		return self.GetInt( "deaths" );
 	}
+	
+	public static void SetLastKillTick( this IClient self, int value )
+	{
+		self.SetInt( "ticks_since_last_kill", value );
+	}
+
+	/// <summary>
+	/// The tick this player last got a kill.
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="value"></param>
+	public static int GetLastKillTick( this IClient self )
+	{
+		return self.GetInt( "ticks_since_last_kill" );
+	}
 }
